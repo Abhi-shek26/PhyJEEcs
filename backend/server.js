a89c7980af523a876b2f9c20baee329812b760be
chiragmalik2612 <chiragmalik2612@gmail.com>
2025-03-21 18:05:43 +0530

    User login-Signup logic done

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,22 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require('./routes/User')
+
 
 const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/user", userRoutes);
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {dbName: "PhyJEEcs"})
   .then(() => {
     console.log("connected to database");
     app.listen(process.env.PORT, () => {
