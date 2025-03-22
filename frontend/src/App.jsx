53a4a56216796f4e61792848a182d7d19b3ff76b
Abhi-shek26 <ak4523817@gmail.com>
2025-03-22 17:06:52 +0530

    responsive login signup page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 function App() {
-
-  return (
-    <>
-      <h1>PhyJEEcs</h1>
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
+
