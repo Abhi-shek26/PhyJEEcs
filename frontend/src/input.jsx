fd4cd28bddcd6fdbf9e551179b773b2f15673d6b
chiragmalik2612 <chiragmalik2612@gmail.com>
2025-03-23 15:40:27 +0530

    resolved merge conflict1

--- frontend/src/input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Input = ({ id, type, label }) => (
-    <input className="form-group__input" type={type} id={id} placeholder={label} required />
-);
-
-export default Input;
