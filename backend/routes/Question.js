9285015ca29896adf6765202bd46ac03a868b583
chiragmalik2612 <chiragmalik2612@gmail.com>
2025-03-26 01:17:41 +0530

    Question Model modified, attempt question route & Controller done, API testing done

--- backend/routes/Question.js
+++ backend/routes/Question.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const upload = require("../middleware/multer");
-const { uploadQuestion, getQuestions } = require("../controllers/Question");
+const { uploadQuestion, getQuestions, attemptQuestion } = require("../controllers/Question");
 
 const router = express.Router();
 
 router.post("/upload", upload.single("image"), uploadQuestion);
 router.get("/questions", getQuestions);
+router.post("/attempt", attemptQuestion);
 
 module.exports = router;
