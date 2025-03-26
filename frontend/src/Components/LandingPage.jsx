c972c43dd5932ad54589e0c78da311ea6276b4a3
Abhi-shek26 <ak4523817@gmail.com>
2025-03-27 00:22:34 +0530

    working on landing page

---
+++ frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to our website!</h1>
+      <p>Here you can find all the information you need.</p>
+    </div>
+  );
+};
+export default LandingPage;
